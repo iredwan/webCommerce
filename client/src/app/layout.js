11af389ed6ceccbd1ab6abb64b188792811ce560
Ifrahim Redwan <110650408+iredwan@users.noreply.github.com>
2025-06-19 12:40:33 +0600

    Update layout.js

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Provider } from "./Provider";
 import AppWrapper from "@/components/AppWrapper"; // client wrapper
 import Toast from "../utils/toast";
-
+import CustomTopLoader from "@/components/CustomTopLoader";
 const poppins = Poppins({ 
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${poppins.className} antialiased bg-gray-50 dark:bg-gray-900`}>
         <Provider>
+          <CustomTopLoader />
           <Toast />
           <AppWrapper>{children}</AppWrapper>
         </Provider>
